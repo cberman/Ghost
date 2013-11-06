19257e4506f85af245205abacc07734cd3a8d592
Collin Berman <cmb5nh@virginia.edu>
2013-11-05 22:33:59 -0500

    Define Location Services callback

--- src/edu/virginia/cs2110/ghost/MainActivity.java
+++ src/edu/virginia/cs2110/ghost/MainActivity.java
@@ -2,18 +2,23 @@
 package edu.virginia.cs2110.ghost;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.Toast;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements
+		GooglePlayServicesClient.ConnectionCallbacks,
+		GooglePlayServicesClient.OnConnectionFailedListener {
 
 	/*
 	 * Define a request code to send to Google Play services This code is
@@ -80,11 +85,28 @@ public class MainActivity extends FragmentActivity {
 			// ...
 		}
 	}
+	
+	private void showErrorDialog(int errorCode) {
+		// Get the error dialog from Google Play services
+		Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+				errorCode, this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+		// If Google Play services can provide an error dialog
+		if (errorDialog != null) {
+			// Create a new DialogFragment for the error dialog
+			ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+			// Set the dialog in the DialogFragment
+			errorFragment.setDialog(errorDialog);
+			// Show the error dialog in the DialogFragment
+			errorFragment.show(getSupportFragmentManager(),
+					"Location Updates");
+		}
+	}
 
 	private boolean servicesConnected() {
 		// If this doesn't compile, setup Services Google Play SDK
 		// http://developer.android.com/google/play-services/setup.html
-		
+
 		// Check that Google Play services is available
 		int resultCode = GooglePlayServicesUtil
 				.isGooglePlayServicesAvailable(this);
@@ -96,21 +118,63 @@ public class MainActivity extends FragmentActivity {
 			return true;
 			// Google Play services was not available for some reason
 		} else {
-			// Get the error dialog from Google Play services
-			Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
-					resultCode, this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
-
-			// If Google Play services can provide an error dialog
-			if (errorDialog != null) {
-				// Create a new DialogFragment for the error dialog
-				ErrorDialogFragment errorFragment = new ErrorDialogFragment();
-				// Set the dialog in the DialogFragment
-				errorFragment.setDialog(errorDialog);
-				// Show the error dialog in the DialogFragment
-				errorFragment.show(getSupportFragmentManager(),
-						"Location Updates");
-			}
+			showErrorDialog(resultCode);
 			return false;
+		}
+	}
+
+	/*
+	 * Called by Location Services when the request to connect the client
+	 * finishes successfully. At this point, you can request the current
+	 * location or start periodic updates
+	 */
+	@Override
+	public void onConnected(Bundle dataBundle) {
+		// Display the connection status
+		Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+
+	}
+
+	/*
+	 * Called by Location Services if the connection to the location client
+	 * drops because of an error.
+	 */
+	@Override
+	public void onDisconnected() {
+		// Display the connection status
+		Toast.makeText(this, "Disconnected. Please re-connect.",
+				Toast.LENGTH_SHORT).show();
+	}
+
+	/*
+	 * Called by Location Services if the attempt to Location Services fails.
+	 */
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		/*
+		 * Google Play services can resolve some errors it detects. If the error
+		 * has a resolution, try sending an Intent to start a Google Play
+		 * services activity that can resolve error.
+		 */
+		if (connectionResult.hasResolution()) {
+			try {
+				// Start an Activity that tries to resolve the error
+				connectionResult.startResolutionForResult(this,
+						CONNECTION_FAILURE_RESOLUTION_REQUEST);
+				/*
+				 * Thrown if Google Play services canceled the original
+				 * PendingIntent
+				 */
+			} catch (IntentSender.SendIntentException e) {
+				// Log the error
+				e.printStackTrace();
+			}
+		} else {
+			/*
+			 * If no resolution is available, display a dialog to the user with
+			 * the error.
+			 */
+			showErrorDialog(connectionResult.getErrorCode());
 		}
 	}
 
