c2618360a23f9a50ae1995d1d7fe06fb5263d154
Collin Berman <cmb5nh@virginia.edu>
2013-11-14 00:08:21 -0500

    Basic View for looking at the camera

---
+++ src/edu/virginia/cs2110/ghost/CameraView.java
@@ -0,0 +1,100 @@
+package edu.virginia.cs2110.ghost;
+
+import java.io.IOException;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Build;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
+	private SurfaceHolder mHolder;
+	private Camera mCamera;
+
+	public CameraView(Context context) {
+		super(context);
+		
+		if(!checkCameraHardware(context))
+			throw new RuntimeException();
+		
+		mCamera = getCameraInstance();
+
+		// Install a SurfaceHolder.Callback so we get notified when the
+		// underlying surface is created and destroyed.
+		mHolder = getHolder();
+		mHolder.addCallback(this);
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB)
+			// deprecated setting, but required on Android versions prior to 3.0
+			mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	}
+
+	public void surfaceCreated(SurfaceHolder holder) {
+		// The Surface has been created, now tell the camera where to draw the
+		// preview.
+		try {
+			mCamera.setPreviewDisplay(holder);
+			mCamera.startPreview();
+		} catch (IOException e) {
+			Log.d("Camera", "Error setting camera preview: " + e.getMessage());
+		}
+	}
+
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		// empty. Take care of releasing the Camera preview in your activity.
+	}
+
+	public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+		// If your preview can change or rotate, take care of those events here.
+		// Make sure to stop the preview before resizing or reformatting it.
+
+		if (mHolder.getSurface() == null) {
+			// preview surface does not exist
+			return;
+		}
+
+		// stop preview before making changes
+		try {
+			mCamera.stopPreview();
+		} catch (Exception e) {
+			// ignore: tried to stop a non-existent preview
+		}
+
+		// set preview size and make any resize, rotate or
+		// reformatting changes here
+
+		// start preview with new settings
+		try {
+			mCamera.setPreviewDisplay(mHolder);
+			mCamera.startPreview();
+
+		} catch (Exception e) {
+			Log.d("Camera", "Error starting camera preview: " + e.getMessage());
+		}
+	}
+	
+	/** Check if this device has a camera */
+	private boolean checkCameraHardware(Context context) {
+	    if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+	        // this device has a camera
+	        return true;
+	    } else {
+	        // no camera on this device
+	        return false;
+	    }
+	}
+	
+	/** A safe way to get an instance of the Camera object. */
+	private static Camera getCameraInstance(){
+	    Camera c = null;
+	    try {
+	        c = Camera.open(); // attempt to get a Camera instance
+	    }
+	    catch (Exception e){
+	        // Camera is not available (in use or does not exist)
+	    }
+	    return c; // returns null if camera is unavailable
+	}
+}
