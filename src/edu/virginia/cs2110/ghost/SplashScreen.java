c52a5a4def314c44d66a06df30d6c149a4ba5f3a
gracemcaden <cgm3kd@virginia.edu>
2013-11-20 16:50:24 -0500

    edited splash screen (java and xml files) and created colors.xml

--- src/edu/virginia/cs2110/ghost/SplashScreen.java
+++ src/edu/virginia/cs2110/ghost/SplashScreen.java
@@ -1,7 +1,27 @@
 package edu.virginia.cs2110.ghost;
 
-public class SplashScreen {
-	
-	
-//change
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+ 
+public class SplashScreen extends Activity {
+ 
+    private static int SPLASH_TIME_OUT = 3000;
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+ 
+        new Handler().postDelayed(new Runnable() {
+ 
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+ 
 }
