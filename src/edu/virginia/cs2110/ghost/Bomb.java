5495280169ebfd4527042760f4916f908cbe9432
msc2ey <matt.chan1@gmail.com>
2013-11-23 16:42:59 -0500

    Update Bomb.java

--- src/edu/virginia/cs2110/ghost/Bomb.java
+++ src/edu/virginia/cs2110/ghost/Bomb.java
@@ -1,5 +1,50 @@
 package edu.virginia.cs2110.ghost;
 
-public class Bomb {
+public class Bomb extends Item {
+
+	private double blastRadius;
+
+	public Bomb(String geofenceId, double latitude, double longitude,
+			float radius, long expiration, int transition) {
+		super(geofenceId, latitude, longitude, radius, expiration, transition);
+		// TODO Auto-generated constructor stub
+	}
+
+	// feed constructor current location of user.
+	public Bomb(String geofenceId, double latitude, double longitude,
+			float radius, long expiration, int transition, double blastRadius) {
+		super(geofenceId, latitude, longitude, radius, expiration, transition);
+		this.setBlastRadius(blastRadius);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GhostStore explode(GhostStore mGhosts, double myLat, double myLong) {
+		for (String id : mGhosts.getIds()) {
+			Ghost g = mGhosts.getGhost(id);
+			double distance = Math.pow(
+					Math.pow(Math.abs(g.getLatitude() - myLat), 2)
+							+ Math.pow(Math.abs(g.getLongitude() - myLong), 2),
+					0.5);
+			if ( distance <= blastRadius) {
+				mGhosts.clearGhost(id);
+				/**
+				 * need to remove ghost from the map
+				 * need to set blastRadius
+				 */
+				
+			}
+		}
+
+		return mGhosts;
+
+	}
+
+	public double getBlastRadius() {
+		return blastRadius;
+	}
+
+	public void setBlastRadius(double blastRadius) {
+		this.blastRadius = blastRadius;
+	}
 
 }
